b6b5175f1f2d21887b37a4a9debe675c596f07ec
Unknown <proffy@comcast.net>
2017-10-23 07:51:52 -0700

    Initial file commit

---
+++ EventConsumer/Program.cs
@@ -0,0 +1,25 @@
+﻿using TimerLib;
+using static System.Console;
+
+namespace EventConsumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Timer timer = new Timer();
+            timer.DivByTen += HandleDivByTen;
+            timer.StartTimer();
+
+            WriteLine("done!");
+            ReadLine();
+        }
+
+        private static void HandleDivByTen(object sender, TimerEventArgs e)
+        {
+            //Timer t = (Timer)sender;
+            //WriteLine($"Hit div by ten: {t.count}");
+            WriteLine($"Hit div by ten: {e.message} count: {e.count}");
+        }
+    }
+}
---
+++ TimerLib/Timer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading;
+
+namespace TimerLib
+{
+    public delegate void TimerHandler(object sender, TimerEventArgs e);
+
+    public class Timer
+    {
+        public event TimerHandler DivByTen;
+        public int count;
+
+        public void StartTimer()
+        {
+            count = 1;
+
+            for(count = 1; count < 100; ++count)
+            {
+                Thread.Sleep(1000);
+
+                if(count%10 == 0)
+                {
+                    DivByTen(this, new TimerEventArgs { message = "foo", count = this.count});
+                }
+            }
+
+        }
+    }
+
+    public class TimerEventArgs : EventArgs
+    {
+        public string message;
+        public int count;
+    }
+}
